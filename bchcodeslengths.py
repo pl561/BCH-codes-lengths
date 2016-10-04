152fdd7d6eea1572b3b25733ea0bc45a63b85ce1
pl561 <pl561@users.noreply.github.com>
2016-10-04 11:49:01 +0200

    Create bchcodeslengths.py

---
+++ bchcodeslengths.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+import os
+import namedtuple
+
+
+
+
+
+
+
+def do():
+  pass
+  
+def main():
+  do()
+  
+if __name__ == "__main__":
+  sys.exit(main())
