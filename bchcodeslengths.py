b9f24ca2c31fc3215207149d798b852c12ac0610
pl561 <pl561@users.noreply.github.com>
2016-10-04 12:11:16 +0200

    class handling bch codes

--- bchcodeslengths.py
+++ bchcodeslengths.py
@@ -5,7 +5,22 @@ import sys
 import os
 import namedtuple
 
+class BCHCode(object):
+    """docstring for BCHCode."""
+    def __init__(self, n, k, t):
+        self.n = n
+        self.k = k
+        self.t = t
 
+    def getErrorRate(self):
+        return float(self.t)/self.n
+
+    def getCodeRate(self):
+        return float(self.k)/self.n
+
+n = 511
+k_values = []
+t_values = []
 
 
 
@@ -13,9 +28,9 @@ import namedtuple
 
 def do():
   pass
-  
+
 def main():
   do()
-  
+
 if __name__ == "__main__":
   sys.exit(main())
